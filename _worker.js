4912cdb4e62ec046d6887e6fa1ba1c13b57b2284
rzkydfr <67826972+rzkydfr@users.noreply.github.com>
2025-10-01 15:21:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // Variables
 const rootDomain = "rzkydfr.workers.dev";
 const serviceName = "rephweb";
-let serviceName = "";
-let APP_DOMAIN = "";
+let serviceName = "rzkydfr.workers.dev"";
+let APP_DOMAIN = "rephweb";
 
 let prxIP = "";
 let cachedPrxList = [];
@@ -17,7 +17,7 @@ const neko = "Y2xhc2g=";
 
 const PORTS = [443, 80];
 const PROTOCOLS = [atob(horse), atob(flash), "ss"];
-const SUB_PAGE_URL = "https://foolvpn.me/nautica";
+const SUB_PAGE_URL = "https://rzkydfr.workers.dev/rephweb";
 const KV_PRX_URL = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/kvProxyList.json";
 const PRX_BANK_URL = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/proxyList.txt";
 const DNS_SERVER_ADDRESS = "8.8.8.8";
